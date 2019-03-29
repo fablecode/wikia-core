40570ca6836a804a5b5e57ba329170320c72f754
fablecode <fablecode@users.noreply.github.com>
2019-03-29 06:02:41 +0000

    Removed uneccessary test and extracted FakeHttpMessageHandler to a seperate file.

--- src/Tests/Unit/wikia.unit.tests/WikiaHttpClientTests.cs
+++ src/Tests/Unit/wikia.unit.tests/WikiaHttpClientTests.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using NSubstitute;
@@ -30,6 +29,7 @@ namespace wikia.unit.tests
         {
             // Arrange
             const string url = "http://good.uri";
+            const string expected = "response";
             var fakeHttpMessageHandler = new FakeHttpMessageHandler(new HttpResponseMessage()
             {
                 StatusCode = HttpStatusCode.OK,
@@ -42,46 +42,7 @@ namespace wikia.unit.tests
             var result = await _sut.GetString(url);
 
             // Assert
-            result.Should().BeEquivalentTo(result);
-        }
-
-
-        [Test]
-        public async Task Given_A_Good_Url_And_Parameters_Should_Return_String_Response()
-        {
-            // Arrange
-            const string url = "http://good.uri";
-            var fakeHttpMessageHandler = new FakeHttpMessageHandler(new HttpResponseMessage()
-            {
-                StatusCode = HttpStatusCode.OK,
-                Content = new StringContent("response", Encoding.UTF8, "application/json")
-            });
-            var fakeHttpClient = new HttpClient(fakeHttpMessageHandler);
-            _httpClientFactory.CreateClient().Returns(fakeHttpClient);
-
-            var parameters = new Dictionary<string, string>();
-            
-            // Act
-            var result = await _sut.GetString(url, parameters);
-
-            // Assert
-            result.Should().BeEquivalentTo(result);
-        }
-
-    }
-
-    public class FakeHttpMessageHandler : DelegatingHandler
-    {
-        private HttpResponseMessage _fakeResponse;
-
-        public FakeHttpMessageHandler(HttpResponseMessage responseMessage)
-        {
-            _fakeResponse = responseMessage;
-        }
-
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            return await Task.FromResult(_fakeResponse);
+            result.Should().BeEquivalentTo(expected);
         }
     }
 }
