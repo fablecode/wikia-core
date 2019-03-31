69d1010014a46e73f5300836ae9db91ec95a7bb9
fablecode <fablecode@users.noreply.github.com>
2019-03-31 18:05:52 +0100

    Activity -> Latest code coverage.

--- src/Tests/Unit/wikia.unit.tests/WikiActivityTests/LatestTests.cs
+++ src/Tests/Unit/wikia.unit.tests/WikiActivityTests/LatestTests.cs
@@ -1,9 +1,8 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
 using NSubstitute;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using wikia.Api;
 using wikia.Configuration;
 using wikia.Enums;
@@ -14,7 +13,7 @@ namespace wikia.unit.tests.WikiActivityTests
 {
     [TestFixture]
     [Category(TestType.Unit)]
-    public class ActivityRequestTests
+    public class LatestTests
     {
         private IWikiActivity _sut;
         private IWikiaHttpClient _wikiaHttpClient;
@@ -28,21 +27,23 @@ namespace wikia.unit.tests.WikiActivityTests
         }
 
         [Test]
-        public void Given_An_ActivityEndpoint_And_A_Null_ActivityRequestParameters_Should_Throw_ArgumentNullException()
+        public async Task Given_An_ActivityRequestParameters_Should_Return_Latest_Activities()
         {
             // Arrange
+            _wikiaHttpClient.GetString(Arg.Any<string>(), Arg.Any<Dictionary<string, string>>()).Returns(@"{ ""items"": [ { ""article"": 342, ""user"": 1426382, ""revisionId"": 4196090, ""timestamp"": 1554048731 } ], ""basepath"": ""https://yugioh.fandom.com"" }");
 
             // Act
-            Func<Task<ActivityResponseResult>> act = () => _sut.ActivityRequest(ActivityEndpoint.LatestActivity, null);
+            var result = await _sut.Latest();
 
             // Assert
-            act.Should().Throw<ArgumentNullException>();
+            result.Items.Should().NotBeEmpty();
         }
 
         [Test]
         public async Task Given_An_ActivityEndpoint_And_ActivityRequestParameters_Should_Invoke_GetString_Once()
         {
             // Arrange
+            _wikiaHttpClient.GetString(Arg.Any<string>(), Arg.Any<Dictionary<string, string>>()).Returns(@"{ ""items"": [ { ""article"": 342, ""user"": 1426382, ""revisionId"": 4196090, ""timestamp"": 1554048731 } ], ""basepath"": ""https://yugioh.fandom.com"" }");
 
             // Act
             await _sut.ActivityRequest(ActivityEndpoint.LatestActivity, new ActivityRequestParameters());
